d401f37e6640ed6ce3f574f5c0a6e9741f5773f9
aklaway <dvazquez@cvc.uab.es>
2014-08-06 08:21:08 +0000

    Odometry.h edited online with Bitbucket. Added some comments

--- src/Odometry.h
+++ src/Odometry.h
@@ -22,18 +22,27 @@ using namespace cv;
 using namespace std;
 
 class Odometry {
-	vector<Mat> imgFeed;
-	vector<KeyPoint> kpFeed;
-	vector<Point3d> points;
-	Rect mask; //constrains the feature detector algorithms to an area of the image
-	int queueSize;
+
+	// Atributes
+	vector<Mat>			imgFeed;	// Queue of images
+	vector<KeyPoint>	kpFeed;		// Queue of key point by image
+	vector<Point3d>		points;		// Queue of ...
+	Rect				mask;		// Constrains the feature detector algorithms to an area of the image
+	int					queueSize;	// Size of the queue
 
 public:
+	// Constructors and destructors
 	Odometry();
-	Odometry(string detector, string descriptor, string matcher);
+	Odometry(const string& detector, const string& descriptor, const string& matcher);
 	virtual ~Odometry();
-	void updateQueue(Mat img, bool showResult);
+	
+	// Functions
+	void updateQueue(Mat img, const bool showResult);
 	Point3d triangulatePos(Point3d point1, Point3d point2, Point3d point3, float dist1, float dist2, float dist3);
+	
+	// Get methods
+	
+	// Set methods
 };
 
 #endif /* ODOMETRY_H_ */
